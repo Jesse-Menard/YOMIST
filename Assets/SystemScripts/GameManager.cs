90e540d1fca23351781a33bf454cbf9fff3b7594
Jesse Menard <jessemenard04@hotmail.com>
2025-12-03 20:44:13 -0500

    MOD: Updated to stable Unity version

---
+++ Assets/SystemScripts/GameManager.cs
@@ -0,0 +1,23 @@
+using UnityEditorInternal;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    static GameManager Instance;
+
+    private void Awake()
+    {
+        
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
